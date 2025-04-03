b42b49885fdf53282e3f3c231d18be7f7a97408e
Utkarsh <utkarshpareek04@gmail.com>
2025-04-03 13:38:18 +0530

    util code discussion

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,22 @@
+const asynchandler=(requesthandler)=>{
+       (res,res,next)=>{
+    Promise.resolve(requesthandler(req,res,next)).catch((err)=>next(err))
+                        }
+}
+
+export {asynchandler}
+
+
+//TRY CATCH APPROACH 
+
+// const asynchandler=(fn)=>async(req,res,next)=>{
+
+//     try {
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             sucess:false,
+//             message:err.message
+//         })
+//     }
+// }
