3b0dd1b0bad1e5b14491ab96307d5ae05b7f6174
Utkarsh <utkarshpareek04@gmail.com>
2025-04-13 01:18:53 +0530

    add routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,16 @@
 console.log("User router loaded");
 
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword,
+     getCurrentUser,
+      getUserChannelProfile,
+       getWatchHistory,
+        loginUser,
+         logoutUser,
+          registerUser,
+           updateAccountDetails,
+            updateAvatar,
+             updateCoverImage } from "../controllers/user.controller.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js"
@@ -30,5 +39,12 @@ router.route("/logout").post(verifyJwt,logoutUser)
 //the functions written in post execute sequentially and next() written in middlewares
 //passes the flag that my work is done now you can execute next function 
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt,changeCurrentPassword)
+router.route("/current-user").get(verifyJwt,getCurrentUser)
+router.route("/update-account").patch(verifyJwt,updateAccountDetails)
+router.route("/avatar").patch(verifyJwt,upload.single("avatar"),updateAvatar)
+router.route("/cover-image").patch(verifyJwt,upload.single("coverImage"),updateCoverImage)
+router.route("/c/:username").get(verifyJwt,getUserChannelProfile)
+router.route("/history").get(verifyJwt,getWatchHistory)
 
 export default router
